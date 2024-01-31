4b7cfbdc0758f2a95b58f73fad870a05da13f8a8
Medeu <medeuserikuly@gmail.com>
2024-01-31 15:02:58 +0600

    homework 6

--- 6/script.js
+++ 6/script.js
@@ -1,1 +1,35 @@
+// Task 1
+// const num1 = 5.4;
+// const num2 = 5.5;
+// const num3 = 5.6;
+
+// // Math.round(x) возвращает число, округлённое к ближайшему целому.
+// console.log(Math.round(num1)); 
+// console.log(Math.round(num2)); 
+// console.log(Math.round(num3)); 
+
+// // Math.ceil(x) округляет число x до ближайшего большего целого числа.
+// console.log(Math.ceil(num1)); // Вывод: 6
+// console.log(Math.ceil(num2)); // Вывод: 6
+// console.log(Math.ceil(num3)); // Вывод: 6
+
+// // Math.floor(x) Округляет аргумент до ближайшего меньшего целого.
+// console.log(Math.floor(num1)); // Вывод: 5
+// console.log(Math.floor(num2)); // Вывод: 5
+// console.log(Math.floor(num3)); // Вывод: 5
+
+
+// Task 2
+// const date = new Date();
+// const hours = date.getHours();
+// const minutes = date.getMinutes();
+
+// const monthOptions = { month: 'long' };
+
+// const formattedDate = date.toLocaleDateString('ru-RU', { day: 'numeric', month: 'long', year: 'numeric' });
+// const formattedTime = `${hours} часов ${minutes} минут`;
+
+// console.log(`Сегодня ${formattedDate}`);
+// console.log(`${formattedTime}`);
+
 
---
+++ 7/script.js
@@ -0,0 +1,17 @@
+
+
+// const ul = document.querySelector('.forRemove');
+// const lists = ul.querySelectorAll('li');
+
+// lists.forEach(list => {
+//     list.textContent = 'sdfsdf'
+// })
+
+// const paragraph = document.createElement('p');
+// paragraph.textContent = 'Hello World!';
+// paragraph.style.cssText = `
+//     font-size: 36px;
+//     font-weight: bold;
+// `;
+// document.body.appendChild(paragraph);
+
